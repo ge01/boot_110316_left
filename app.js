e50f9086d3a7addc749cdd2dcf18f5c048dd2870
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-03 22:35:52 -0500

    function

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
+/*
 var integer = 4;
 var string = "camel";
 var holdWord = [];
 var stringWord = "";
-
-holdWord = string.split('');
-for(var i=0; i < integer; i++){
-  holdWord.push("0");
-}
-stringWord = holdWord.join('');
-
-
-console.log(stringWord);
-
-/*
-string = "a";
-repeatSTring = string.repeat(4);
-
-test = "bird";
-
-console.log(repeatSTring + test);
-
-console.log(test + repeatSTring);
 */
+
+function me(string, padint) {
+  var holdWord = [];
+  holdWord = string.split('');
+  //console.log(holdWord);
+  for(var i=0; i < padint; i++){
+    holdWord.push("0");
+  }
+
+  stringWord = holdWord.join('');
+  return stringWord;
+
+  //stringWord = holdWord.join('');
+}
+
+console.log(me("camel", 4));
+
+
+//console.log(stringWord);
